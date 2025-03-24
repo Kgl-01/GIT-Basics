eca42d5aa1148ca7932566f9831a372748080d8f
Kgl-01 <karthikgowda.mailme@gmail.com>
2025-03-24 21:34:06 +0530

    handling user input in a better way for single word input without any spaces

--- investment_calculator.go
+++ investment_calculator.go
@@ -11,12 +11,18 @@ func main() {
 	years := 10.0
 	expectedReturnRate := 5.5
 
+	fmt.Print("Investment Amount: $")
 	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Years: ")
+	fmt.Scan(&years)
+
+	fmt.Print("Expected Return Rate: ")
+	fmt.Scan(&expectedReturnRate)
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
 	fmt.Println(futureValue)
 	fmt.Println(futureRealValue)
-	fmt.Printf("investment amount value, %s", investmentAmount)
 }
