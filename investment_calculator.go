cad2f61786445f97694393148fbad989841fd20b
Kgl-01 <karthikgowda.mailme@gmail.com>
2025-03-24 21:05:43 +0530

    alternative variable declare style

--- investment_calculator.go
+++ investment_calculator.go
@@ -1,9 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Print(`Hello World`)
+	investmentAmount, years := 1000.0, 10.0
+	expectedReturnRate := 5.5
+
+	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+
+	fmt.Println(futureValue)
 }
-
-
